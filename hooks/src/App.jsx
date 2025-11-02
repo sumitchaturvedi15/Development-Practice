6fd7134be854cb56f9184e29b36ada087850e6fa
Sumit Chaturvedi <sumitchaturvedilucky2002@gmail.com>
2025-11-03 01:40:04 +0530

    Basic Practice

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const addValue=()=>{
+    setCount(count+1);
+  }
+  const removeValue=()=>{
+    if(count>0){
+      setCount(count-1);
+    }
+    else{
+      alert("Count cannot be negative");
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Counter {count}</p>
+      <button onClick={addValue}>Add +1 on {count}</button>
+      <br />
+      <button onClick={removeValue}>Remove 1 from {count}</button>
     </>
   )
 }
